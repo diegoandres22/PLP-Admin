5828ba71dbcb679e03393684128053f16110a34f
diegoandres22 <diego.a.v3005@gmail.com>
2025-07-07 20:36:28 -0400

    SLINT: page

--- src/app/newRaffle/page.tsx
+++ src/app/newRaffle/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NewRaffle } from '@/component/1-screens'
 
-export default function NewRaffleApp() {
+export default function page() {
     return (
         <div className="w-full h-[89vh] pl-4 sm:pl-52 sm:py-4 pr-4 mt-2 sm:mt-0">
             <NewRaffle />

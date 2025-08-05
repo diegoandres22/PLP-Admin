7328f3a16c560573ae34b813f67bb658045204b5
diegoandres22 <diego.a.v3005@gmail.com>
2025-08-04 21:48:33 -0400

    FEAT: agregar protección de rutas en AprobationsScreen y NewRaffleScreen, y mejorar el manejo de sesión en ButtomsNextAuth

--- src/component/1-screens/aprobaciones/aprobationsScreen.tsx
+++ src/component/1-screens/aprobaciones/aprobationsScreen.tsx
@@ -1,9 +1,19 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AprobationsSection } from '@/component/2-sections'
 import { Badge } from '@heroui/react'
+import { useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
 
 export const AprobationsScreen = () => {
+    const { status } = useSession();
+
+    useEffect(() => {
+        if (status === "unauthenticated") {
+            redirect("/");
+        }
+    }, [status]);
+
     return (
         <div className="w-full h-full flex flex-col bg-gray-800 rounded-lg p-10 gap-5">
 
--- src/component/1-screens/newRaffle/newRaffleScreen.tsx
+++ src/component/1-screens/newRaffle/newRaffleScreen.tsx
@@ -1,7 +1,19 @@
+"use client"
+import React, { useEffect } from 'react'
 import { NewRaffleSection } from '@/component/2-sections'
-import React from 'react'
+import { useSession } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 
 export const NewRaffleScreen = () => {
+
+    const { status } = useSession();
+    
+        useEffect(() => {
+            if (status === "unauthenticated") {
+                 redirect("/");
+            }
+        }, [status]);
+        
     return (
         <div className="w-full h-full flex flex-col bg-gray-800 rounded-lg p-10 gap-10 max-h-[87vh] overflow-auto">
             <h3 className=' text-2xl font-bold flex '>Nueva Rifa</h3>

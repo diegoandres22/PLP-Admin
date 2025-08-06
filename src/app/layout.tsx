cde804ef38b9b9041b161b9a3901d1d33ee63d0b
diegoandres22 <diego.a.v3005@gmail.com>
2025-08-06 19:08:29 -0400

    FEAT: agregar LayoutShell para gestionar la estructura de la página y mejorar la navegación

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ProviderComp } from "@/store/provider";
+import { LayoutShell } from "@/component/layoutShell";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Administrador",
-  description: "Gestiona rifas, compras y estadísticas y más",
+  description: "Gestiona rifas, compras, estadísticas y más",
 };
 
 export default function RootLayout({
@@ -27,9 +28,45 @@ export default function RootLayout({
     <html lang="es" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ProviderComp>
-          {children}
+          <LayoutShell>{children}</LayoutShell>
         </ProviderComp>
       </body>
     </html>
   );
 }
+
+// import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+// import { ProviderComp } from "@/store/provider";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// export const metadata: Metadata = {
+//   title: "Administrador",
+//   description: "Gestiona rifas, compras y estadísticas y más",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="es" className="dark">
+//       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+//         <ProviderComp>
+//           {children}
+//         </ProviderComp>
+//       </body>
+//     </html>
+//   );
+// }

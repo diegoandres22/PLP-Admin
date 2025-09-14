f413573ae3ca314f86f05e62a05ccfc3e6d25b0a
diegoandres22 <diego.a.v3005@gmail.com>
2025-09-13 23:58:29 -0400

    FEAT: agrego metodo post para creación de rifa con toast

--- src/store/slices/rafflesSlice.ts
+++ src/store/slices/rafflesSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { Raffle, RafflesState } from "@/types";
-import { getRaffles } from "../services/raffleService";
+import { getRaffles, createRaffleService } from "../services/raffleService";
+import { addToast } from "@heroui/react";
 
 // thunk para obtener las rifas
 export const fetchRaffles = createAsyncThunk<Raffle[]>(
@@ -10,6 +11,45 @@ export const fetchRaffles = createAsyncThunk<Raffle[]>(
   }
 );
 
+// thunk para crear una rifa
+export const createRaffle = createAsyncThunk<
+  Raffle,
+  FormData,
+  { rejectValue: string }
+>("raffles/create", async (formData, { rejectWithValue }) => {
+  try {
+    const response = await createRaffleService(formData);
+
+    addToast({
+      title: "Rifa creada 🎉",
+      description: "La rifa se ha creado exitosamente",
+      timeout: 3000,
+      color:"success"
+    });
+
+    return response; 
+  } catch (error) {
+    let message = "Error al crear la rifa";
+
+    if (
+      typeof error === "object" &&
+      error !== null &&
+      "response" in error &&
+      typeof (error as { response?: { data?: { message?: string } } }).response?.data?.message === "string"
+    ) {
+      message = (error as { response: { data: { message: string } } }).response.data.message;
+    }
+
+    addToast({
+      title: "Error ❌",
+      description: message,
+      timeout: 5000,
+      color:"danger"
+    });
+
+    return rejectWithValue(message); // devuelve rejectValue
+  }
+});
 
 const initialState: RafflesState = {
   raffles: [],
@@ -23,6 +63,7 @@ const raffleSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
+      // fetch
       .addCase(fetchRaffles.pending, (state) => {
         state.loading = true;
         state.error = null;
@@ -33,10 +74,65 @@ const raffleSlice = createSlice({
       })
       .addCase(fetchRaffles.rejected, (state, action) => {
         state.loading = false;
-        state.error = action.error.message || "Error al obtener rifas";
+        state.error = action.payload as string;
+      })
+      // create
+      .addCase(createRaffle.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createRaffle.fulfilled, (state, action) => {
+        state.loading = false;
+        state.raffles.push(action.payload);
+      })
+      .addCase(createRaffle.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload as string;
       });
   },
 });
 
 export default raffleSlice.reducer;
+
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import { Raffle, RafflesState } from "@/types";
+// import { getRaffles } from "../services/raffleService";
+
+// // thunk para obtener las rifas
+// export const fetchRaffles = createAsyncThunk<Raffle[]>(
+//   "raffles/fetchAll",
+//   async () => {
+//     return await getRaffles();
+//   }
+// );
+
+
+// const initialState: RafflesState = {
+//   raffles: [],
+//   loading: false,
+//   error: null,
+// };
+
+// const raffleSlice = createSlice({
+//   name: "raffles",
+//   initialState,
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchRaffles.pending, (state) => {
+//         state.loading = true;
+//         state.error = null;
+//       })
+//       .addCase(fetchRaffles.fulfilled, (state, action) => {
+//         state.loading = false;
+//         state.raffles = action.payload;
+//       })
+//       .addCase(fetchRaffles.rejected, (state, action) => {
+//         state.loading = false;
+//         state.error = action.error.message || "Error al obtener rifas";
+//       });
+//   },
+// });
+
+// export default raffleSlice.reducer;
 
